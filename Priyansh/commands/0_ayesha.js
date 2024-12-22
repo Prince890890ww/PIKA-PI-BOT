a3c2a280ad1e24b07cc56eefd4f64d2563c612b2
9253Raza <razaji92535982@gmail.com>
2024-12-22 20:05:40 +0530

    Update 0_ayesha.js

--- Priyansh/commands/0_ayesha.js
+++ Priyansh/commands/0_ayesha.js
@@ -14,11 +14,11 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("Ayesha")==0 || event.body.indexOf("AYESHA")==0 || event.body.indexOf("ayesha")==0 || event.body.indexOf("@Ayesha Khan")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻   𝙆𝙄 𝙒𝙄𝙁𝙀 \n__________________________________\n\n𝘼𝙔𝙀𝙎𝙃𝘼 𝙄𝙈𝙍𝘼𝙉 𝙆𝙃𝘼𝙉 𝙆𝙄 𝙒𝙄𝙁𝙀 𝙃𝘼𝙄 👇👇👇 \n__________________________________ ",
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻   𝙆𝙄 𝙒𝙄𝙁𝙀 __________________________________\n\n𝘼𝙔𝙀𝙎𝙃𝘼 𝙄𝙈𝙍𝘼𝙉 𝙆𝙃𝘼𝙉 𝙆𝙄 𝙒𝙄𝙁𝙀 𝙃𝘼𝙄 👇👇👇 \n__________________________________ ",
                                 attachment: fs.createReadStream(__dirname + `/noprefix/ayesha.jpg`)
                         }
                         api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("🤗", event.messageID, (err) => {}, true)
+    api.setMessageReaction("😘", event.messageID, (err) => {}, true)
                 }
         }
         module.exports.run = function({ api, event, client, __GLOBAL }) {
